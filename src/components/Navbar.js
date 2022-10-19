f40b05f152b5733a81e548c5aee5ca1b2bbce9ae
tinashe <karimugwagwanorkias@gmail.com>
2022-10-19 23:53:34 +0200

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import logo from '../images/logo.png'
 
 function Navbar() {
   return (
-    <ul>
-        {SidebarData.map((item, index) => {
-return (
-  <li key={index} className={item.cName}>
-    <Link to={item.path} >
-      <span className='sidebar-item'>{item.icon}</span>
-      <span className='sidebar-item'>{item.title}</span>
-    </Link>
-  </li>
-);
-})}
-    </ul>
+              <aside >
+              <div className='top'>
+                <div className='logo'>
+                  <img src={logo} alt=''/>
+                  <h2>NK<span className='trouble'>Brand</span></h2>
+                </div>
+                <div className='close' id='close-btn'>
+                <AiIcons.AiOutlineClose/>
+                </div>
+              </div>
+
+              <div className='sidebar'> 
+
+              {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path} >
+                    <span className='sidebar-item'>{item.icon}</span>
+                    <span className='sidebar-item'>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+
+              </div>
+
+
+
+            </aside>  
   )
 }
 
