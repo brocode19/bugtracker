0ca8d7db43036ae331f3266a965a85adafb906fb
tinashe <karimugwagwanorkias@gmail.com>
2022-10-20 15:14:54 +0200

    added chart js

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import 'chart.js/auto'
 
 
 
@@ -6,9 +8,36 @@ function Dashboard() {
   return (
     <div className='pages'>
 
+      <Container>
+        <Row className='graph-content'>
+          <Col sm={12} lg={8}>
+            <div className='graphs container'>
+              <div className='row '>
+                <div className='col-4'>
+                  <div className='graph'>graph 1</div>
+                </div>
+                <div className='col-4'>
+                <div className='graph'>graph 1</div>
+                </div>
+                <div className='col-4'>
+                <div className='graph'>graph 1</div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} lg={4}>
+            <div className='line-graph-container container'>
+              <div className='ine-graph'>hey</div>
+              
+            </div>
+          </Col>
+        </Row>
+        <Row>dash2</Row>
+      </Container>
+
 
       
-      Dashboard
+
       </div>
   )
 }
