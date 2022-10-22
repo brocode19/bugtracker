2b4dc82a39db51083968d1c4a03a796ab7597565
tinashe <karimugwagwanorkias@gmail.com>
2022-10-22 16:26:46 +0200

    typograpy and container

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,18 @@
+
+import { Container, Typography } from '@mui/material'
 import React from 'react'
 
 function Projects() {
   return (
-    <div className='pages'>Projects</div>
+    <div className='pages'>
+      <Container maxWidth='xl'>
+      <Typography variant="h4" sx={{ mb: 5 }}>
+          Hi, Welcome back Tanashe
+        </Typography>
+       
+      </Container>
+
+    </div>
   )
 }
 
